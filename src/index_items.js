3b259c0b2ce1c4da050003dd433d6dd84d6a89cc
Rinkue <94703528+sr-rinkue@users.noreply.github.com>
2026-03-07 20:47:19 +0100

    Update 2

--- src/index_items.js
+++ src/index_items.js
@@ -6,7 +6,7 @@ const items = [
       "You are one of the ten people who have been invited to play a game, which takes place in a town you don't know... or maybe you do?",
     fullDesc:
       "<h3>ARE YOUR FRIENDS REALLY YOUR FRIENDS?</h3><p>Inspired by the card game <em>Les Loups-garous de Thiercelieux</em>, in <em>The Town</em> you will join seven other people in an experience where you will not know who is lying or telling the truth about the murders that occur at night.</p><br><h3>A GAME YOU CAN PLAY BY YOURSELF</h3><p>For the first time ever, you won't have to call your friends to play with you, thanks to the AI that is part of RinStation. For people who don't have friends, this is a great achievement!</p>",
-    tags: [{ icon: "simple-icons:windows", text: "FOR WINDOWS 10 AND 11" }],
+    tags: [{ icon: "simple-icons:windows", text: "FOR WINDOWS 10 AND 11" }, { icon: "simple-icons:android", text: "FOR ANDROID" }],
     externalLink: {
       text: "Listen to The Town OST",
       icon: "ri-music-line",
